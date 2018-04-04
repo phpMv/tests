c379d6d5bba43541d44e9cd90ce331dc22ec04be
Jean Christophe Heron <heronj@campus.unicaen.fr>
2018-04-04 11:53:44 +0200

    First commit

---
+++ app/config/config.php
@@ -0,0 +1,29 @@
+<?php
+return array(
+		"siteUrl"=>"http://127.0.0.1/tests/",
+		"database"=>[
+				"type"=>"mysql",
+				"dbName"=>"",
+				"serverName"=>"127.0.0.1",
+				"port"=>"3306",
+				"user"=>"root",
+				"password"=>"",
+				"cache"=>false
+		],
+		"sessionName"=>"tests",
+		"namespaces"=>[],
+		"templateEngine"=>'Ubiquity\\views\\engine\\Twig',
+		"templateEngineOptions"=>array("cache"=>false),
+		"test"=>false,
+		"debug"=>false,
+		"di"=>["jquery"=>function($controller){
+							$jquery=new \Ajax\php\ubiquity\JsUtils(["defer"=>true],$controller);
+							$jquery->semantic(new \Ajax\Semantic());
+							return $jquery;
+						}],
+		"cache"=>["directory"=>"cache/","system"=>"Ubiquity\\cache\\system\\ArrayCache","params"=>[]],
+		"mvcNS"=>["models"=>"models","controllers"=>"controllers","rest"=>"rest"],
+		"isRest"=>function(){
+			return \Ubiquity\utils\http\URequest::getUrlParts()[0]==="rest";
+		}
+);
